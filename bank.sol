517a283d17e6fe72713b9310c42b0c8a13164abf
JYuniform64 <jiny2048@gmail.com>
2020-12-10 15:09:05 +0800

    在struct中加入valid项，重构transfer和loan

--- bank.sol
+++ bank.sol
@@ -24,6 +24,7 @@ contract Bank
         address address_to;  //合同的to方
         uint total;  //合同总额度
         uint used;  //被使用的额度
+        bool valid; //合同是否有余额
     }
     Trust_contract[] public contracts;
 
@@ -45,19 +46,16 @@ contract Bank
         if(bank == 0x0000000000000000000000000000000000000000) return ; //判断银行是否存在
         if(end_time < now_time) return ; //判断时间是否合理
         //添加信任合同
-        Trust_contract memory t = Trust_contract(now_time, end_time, bank, msg.sender, money, 0);
+        Trust_contract memory t = Trust_contract(now_time, end_time, bank, msg.sender, money, 0, true);
         contracts.push(t);
     }
 
-    //应收账款的转让
-    function transfer(address payee, uint money) public returns (bool) {
-        require(payee == bank, "Can't transfer to bank");
-
-        //检查额度是否充足
+    //检查账户中应收账款的总量是否大于某个值
+    function check_balance_enough(address a, uint money) private returns (bool) {
         uint total_available = 0;
         bool enough = false;
-        for(uint i = 0; i < constructor.length; i++) {
-            if(constructor[i].payee == msg.sender) 
+        for(uint i = 0; i < contracts.length; i++) {
+            if(contracts[i].to == a) 
                 total_available += (contracts[i].total - contracts[i].used);
             if(total_available >= money) {
                 enough = true;
@@ -66,50 +64,44 @@ contract Bank
         }
         if(total_available < money)
             return false;
-        
+        else 
+            return true;
+    }
+    
+    //应收账款的转让
+    function transfer(address payee, uint money) public returns (bool) {
+        require(payee == bank, "Can't transfer to bank");
+        if(check_balance_enough(msg.sender, money) == false)
+            return false;
+
         //开始转让，遍历所有合同，扣除转让方持有合同中的余额
-        for(uint i = 0; i < constructor.length; i++) {
-            if(contracts[i].address_to == msg.sender) {
-                //如果一份合同的余额足够，直接给这份合同的used加上money，并新建一份额度为money的普通合同，结束遍历
+        for(uint i = 0; i < contracts.length; i++) {
+            if(contracts[i].valid && contracts[i].address_to == msg.sender) {
+                //如果一份合同的余额足够，则扣除后结束遍历
                 if(contracts[i].total - contracts[i].used >= money) {
                     contracts[i].used += money;
-                    Trust_contract memory t = Trust_contract(now_time, contracts[i].end_time, contracts[i].address_from, payee, money, 0);
+                    Trust_contract memory t = Trust_contract(now_time, contracts[i].end_time, contracts[i].address_from, payee, money, 0, true);
                     contracts.push(t);
                     break;
-                } else {
+                } else {    //如果一份合同的余额不足，则扣除后继续遍历
                     money -= contracts[i].total - contracts[i].used;
-                    Trust_contract memory t = Trust_contract(now_time, contracts[i].end_time, contracts[i].address_from, payee, contracts[i].total-contracts[i].used, 0);
+                    Trust_contract memory t = Trust_contract(now_time, contracts[i].end_time, contracts[i].address_from, payee, contracts[i].total-contracts[i].used, 0, true);
                     contracts.push(t);
                     contracts[i].used = contracts[i].total;
+                    contracts[i].valid = false;
                 }
             }
         }
         return true;
     }
 
-    //企业从银行贷款或者融资的函数
-    function load(uint money)public
-    {
-        //判断交易的to方是不是bank，如果是则终止交易
-        if(msg.sender == bank) return ;
-        //判断交易的发起者信用度，如果小于60，则终止交易
-        if(credit[msg.sender] < 60) return ;
-        uint mount = 0; //mount用于储存总余额
-        bool flag = false;
-        for(uint i = 0; i < contracts.length; i++)
-        {
-            if(contracts[i].address_to == msg.sender)
-                mount += (contracts[i].total - contracts[i].used);
-            if(mount >= money)
-            {
-                flag = true;
-                break;
-            }
-        }
-        if(flag == false) return; //余额不足终止交易
+    //企业从银行融资.注意这里的融资仅依据企业持有的应收账款额度，不依赖于企业本身的信用度。
+    function loan(uint money)public returns (bool) {
+        require(msg.sender != bank, "Illegal loan as bank!");
+        if(check_balance_enough(msg.sender, money) == false)
+            return false;
 
-        //开始贷款，遍历所有合同，合同的to方才有权利使用这份余额
-        //money是用户的输入值，代表msg.sender想贷款的额度
+        //遍历所有合同，
         for(uint i = 0; i < contracts.length; i++)
         {
             if(contracts[i].address_to == msg.sender)
