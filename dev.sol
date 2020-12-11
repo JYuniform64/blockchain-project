8b92ae196d18a74cd4a17f554b8efdf1530f4372
JYuniform64 <jiny2048@gmail.com>
2020-12-11 15:18:08 +0800

    增加代码注释，receipt状态变化说明

--- dev.sol
+++ dev.sol
@@ -4,27 +4,28 @@ contract Supply {
     enum e_ApproveStatus { WaittingDebtee, WaittingBank, Approved, Disapproved, WaittingTransfer, Loaned }
     enum e_ReceiptStatus { Valid, Invalid }
     struct Receipt {
-        uint amount;
-        address debtor;
-        address debtee;
-        uint start_time;
-        uint duration;
-        string remark;
-        e_ApproveStatus approval;
-        e_ReceiptStatus status;
+        uint amount;        //金额
+        address debtor;     //债务人
+        address debtee;     //债权人
+        uint start_time;        //单据签订时间
+        uint duration;      //债务有效期
+        string remark;      //单据备注
+        e_ApproveStatus approval;       //单据当前审批状态
+        e_ReceiptStatus status;     //单据当前是否可用于交易
     }
 
-    address public bank;
-    mapping(uint => Receipt) receipts;
-    uint receipt_num;
-    mapping(address => uint) reputations;
-    address[] enterprises;
+    address public bank;        //第三方机构（银行）
+    mapping(uint => Receipt) receipts;      //由序号索引的单据
+    uint receipt_num;       //总单据数量
+    mapping(address => uint) reputations;       //企业信誉度
+    address[] enterprises;      //已向第三方注册的企业
 
     constructor() public {
         bank = msg.sender;
         receipt_num = 0;
     }
 
+    //企业向第三方信用机构注册自己
     function register() public returns (bool success) {
         require(msg.sender != bank, "No need for register");
         bool need_register = true;
@@ -40,7 +41,8 @@ contract Supply {
         } else 
             return false;
     }
-    
+
+    //第三方信用机构对企业的信用进行评级
     function set_reputation(address user, uint value) public returns (bool success) {
         require(msg.sender == bank, "No privilege");
         if (value <=0 || value >= 100)
@@ -54,6 +56,7 @@ contract Supply {
         return reputations[user];
     }
 
+    //信用度对应的最大应收账款单据额度
     function limit_amount_by_reputation(uint reputation) public returns (uint amount) {
         //reputation range[0,100]
         //[81,100]: 一千一百万 - 一亿一千一百万
@@ -65,11 +68,13 @@ contract Supply {
         else if(reputation > 80 && reputation <= 100) return 11000000 + (reputation - 80) * 5000000;
     }
 
+    //判断企业的信用度是否足够使第三方信用机构同意由其发起的应收账款单据
     function enough_reputation(address user, uint amount) public returns (bool) {
         if(reputations[user] < amount) return false;
         else return true;
     }
     
+    //企业debtor向企业debtee发起签署应收账款单据
     function purchase(address debtee, uint amount, uint duration, string remark) public returns (uint index) {
         require(msg.sender != bank, "No privilege");
         Receipt memory receipt = Receipt(
@@ -87,6 +92,7 @@ contract Supply {
         return receipt_num - 1;
     }
 
+    //企业debtee同意企业debtor发起的应收账款单据
     function accept_purchase(uint indexOfReceipt) public {
         require(indexOfReceipt < receipt_num, "Invalid index");
         require(msg.sender == receipts[indexOfReceipt].debtee, "No privilege");
@@ -97,6 +103,7 @@ contract Supply {
         receipts[indexOfReceipt].status = e_ReceiptStatus.Invalid;
     }
 
+    //企业debtee拒绝企业debtor发起的应收账款单据
     function reject_purchase(uint indexOfReceipt) public {
         require(indexOfReceipt < receipt_num, "Invalid index");
         require(msg.sender == receipts[indexOfReceipt].debtee, "No privilege");
@@ -106,6 +113,7 @@ contract Supply {
         receipts[indexOfReceipt].status = e_ReceiptStatus.Invalid;
     }
 
+    //第三方信用机构批准由企业debtor发起，经企业debtee同意的应收账款单据
     function approve_purchase(uint indexOfReceipt) public {
         require(indexOfReceipt < receipt_num, "Invalid index");
         require(msg.sender == receipts[indexOfReceipt].debtee, "No privilege");
@@ -118,6 +126,7 @@ contract Supply {
         receipts[indexOfReceipt].status = e_ReceiptStatus.Valid;
     }
 
+    //第三方信用机构拒绝由企业debtor发起，经企业debtee同意的应收账款单据
     function disapprove_purchase(uint indexOfReceipt) public {
         require(indexOfReceipt < receipt_num, "Invalid index");
         require(msg.sender == receipts[indexOfReceipt].debtee, "No privilege");
@@ -128,6 +137,7 @@ contract Supply {
         receipts[indexOfReceipt].status = e_ReceiptStatus.Invalid;
     }
 
+    //应收账款单据的debtee向一个新的企业转让其持有的应收账款单据的部分或全部额度
     function transfer_receipt(address debtee, uint indexOfReceipt, uint amount, string remark) public returns (uint indexOfOriginReceipt, uint indexOfNewReceipt) {
         require(indexOfReceipt < receipt_num, "Invalid index");
         require(msg.sender == receipts[indexOfReceipt].debtee, "No privilege");
@@ -157,6 +167,7 @@ contract Supply {
         return;
     }
 
+    //检查两个应收账款单据是否同源
     function from_same_receipt(uint index1, uint index2) public returns (bool) {
         //assume both indexs are valid.
         if(receipts[index1].debtor == receipt[index2].debtor && 
@@ -165,6 +176,7 @@ contract Supply {
         else return false;
     }
     
+    //新的企业同意应收账款单据的转让
     function accept_transfer(uint indexOfOriginReceipt, uint indexOfNewReceipt) public {
         require(indexOfReceipt < receipt_num && indexOfOriginReceipt < receipt_num, "Invalid index");
         require(from_same_receipt(indexOfOriginReceipt, indexOfNewReceipt), "Not from same receipt");
@@ -177,6 +189,7 @@ contract Supply {
         receipts[indexOfNewReceipt].status = e_ReceiptStatus.Valid;
     }
 
+    //新的企业拒绝应收账款单据的转让
     function reject_transfer(uint indexOfOriginReceipt, uint indexOfNewReceipt) public {
         //return the money in new receipt back to the origin receipt
         require(indexOfReceipt < receipt_num && indexOfOriginReceipt < receipt_num, "Invalid index");
@@ -192,6 +205,7 @@ contract Supply {
         receipts[indexOfOriginReceipt].status = e_ReceiptStatus.Valid;
     }
 
+    //企业依据其持有的应收账款单据向银行贷款
     function loan(uint indexOfReceipt) public {
         require(indexOfReceipt < receipt_num, "Invalid index");
         require(msg.sender == bank, "No privilege");
@@ -203,6 +217,7 @@ contract Supply {
         receipts[indexOfReceipt].status = e_ReceiptStatus.Invalid;
     }
 
+    //在应收账款单据的债务人缴清费用后删除该单据
     function destoryReceipt(uint indexOfReceipt) public returns (bool success) {
         //Todo: what to do when time expired
         require(indexOfReceipt < receipt_num, "Invalid index");
